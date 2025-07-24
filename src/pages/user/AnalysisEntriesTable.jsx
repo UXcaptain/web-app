c706dc6e23922a47d05aec6520f881cf8b5d1782
Sergio N <s.navarroredondo@gmail.com>
2025-07-24 02:23:35 +0200

    Adds functionality to view analysis entry URLs in AnalysisEntriesTable component

--- src/pages/user/AnalysisEntriesTable.jsx
+++ src/pages/user/AnalysisEntriesTable.jsx
@@ -1,9 +1,23 @@
 import { Table } from '@mantine/core';
 import { useNavigate } from "react-router";
+import apiClient from '../../config/API/axiosConfig.mjs';
 
 export const AnalysisEntriesTable = (props) => {
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
+
+    const handleViewAnalysisEntry = async (analysisEntryId) => {
+
+        try {
+            const analysisEntryUrl = await apiClient.get(`/api/v1/entry/${analysisEntryId}`);
+
+            const followableUrl = analysisEntryUrl.data.analysisEntryUrl
+            window.open(followableUrl, '_blank')
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
 
     const participantsArray = props.participants ? props.participants : [];
 
@@ -15,7 +29,9 @@ export const AnalysisEntriesTable = (props) => {
             <Table.Td>{item.ParticipantsProfile.country}</Table.Td>
 
             
-            <Table.Td><button onClick={() => navigate(`/entry/${item.id}`)}>Play recording</button> </Table.Td>
+            {/* <Table.Td><button onClick={() => navigate(`/entry/${item.id}`)}>Play recording</button> </Table.Td> */}
+
+            <Table.Td><button onClick={() => handleViewAnalysisEntry(item.id)}> view analysis entry</button></Table.Td>
         
         </Table.Tr>
     )) : [];
