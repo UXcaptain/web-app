8bce3c26f62b004489cd7b60d1ea3be37e62d2af
Sergio N <s.navarroredondo@gmail.com>
2025-08-05 22:35:34 +0200

    Enables PostHog integration with debug mode off
    Moves posthog initialization configuration to where it should be
    
    This reorganizes the logic for initializing PostHog in the React application so that the debug mode is explicitly set to false by default.

--- src/main.jsx
+++ src/main.jsx
@@ -28,10 +28,11 @@ import posthog from 'posthog-js';
 import { PostHogProvider } from 'posthog-js/react'
 
 // posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
-//   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-//   defaults: '2025-05-24',
-//   debug: false,
-// });
+posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-05-24',
+  debug: false,
+});
 
 
 createRoot(document.getElementById('root')).render(
