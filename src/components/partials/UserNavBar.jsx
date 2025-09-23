5c947490de2137b03bf3b4579242e32583528fd4
Sergio N <s.navarroredondo@gmail.com>
2025-09-23 23:37:45 +0200

    update nav bar, perfil --> cambiar contraseña

--- src/components/partials/UserNavBar.jsx
+++ src/components/partials/UserNavBar.jsx
@@ -33,7 +33,7 @@ const UserNavBar = () => {
 
   const data = [
   { link: '/dashboard', label: 'Dashboard', icon: IconHomeFilled },
-  { link: '/user/profile', label: 'Perfil', icon: IconUserCircle },
+  { link: '/user/profile', label: 'Cambiar contraseña', icon: IconUserCircle },
   { link: '/user/billing', label: 'Planes', icon: IconCreditCardFilled },
 ];
 
