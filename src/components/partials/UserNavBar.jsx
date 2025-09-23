81486bd17984dc31c0372f9356725686db21e41f
Sergio N <s.navarroredondo@gmail.com>
2025-09-23 23:57:41 +0200

    DISABLE access to billing section - will launch as free beta

--- src/components/partials/UserNavBar.jsx
+++ src/components/partials/UserNavBar.jsx
@@ -34,7 +34,7 @@ const UserNavBar = () => {
   const data = [
   { link: '/dashboard', label: 'Dashboard', icon: IconHomeFilled },
   { link: '/user/profile', label: 'Cambiar contraseña', icon: IconUserCircle },
-  { link: '/user/billing', label: 'Planes', icon: IconCreditCardFilled },
+  // { link: '/user/billing', label: 'Planes', icon: IconCreditCardFilled },
 ];
 
   const links = data.map((item) => (
