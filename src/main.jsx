267e6b40e22f8546a130471c074a796b5c8e0ebf
Sergio N <s.navarroredondo@gmail.com>
2025-09-24 00:42:55 +0200

    Removes redundant route in main.jsx
    Updates routes to handle navigation based on '/' rather than '/waitlist'
    Simplifies routing structure

--- src/main.jsx
+++ src/main.jsx
@@ -44,10 +44,10 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
 
-        <Route path="/waitlist" element={<Waitlist />} /> //* DELETE after launch
+        <Route path="/" element={<Waitlist />} /> //* DELETE after launch
 
-        <Route path="/" element={<Waitlist />}> //* DELETE after launch
-        {/* <Route path="/" element={<Waitlist />}> */} //* UNCOMMENT after launch
+        <Route path="/home" element={<IndexWrapper />}> //* DELETE after launch
+        {/* <Route path="/" element={<IndexWrapper />}> //* UNCOMMER after launch */}
 
           <Route index element={<Homepage />} />
           
