662f1fad4dfae80ffb7c20c589d54c60bf3ee21c
Sergio N <s.navarroredondo@gmail.com>
2025-10-04 14:22:52 +0200

    MINOR text changes

--- src/components/auth/CustomerRegistrationForm.jsx
+++ src/components/auth/CustomerRegistrationForm.jsx
@@ -29,11 +29,11 @@ const handleRegister = async (e) => {
     }
     
     try {
-        const registerResponse = await apiClient.post(`/api/v1/auth/register/local/customer`, data)
+        await apiClient.post(`/api/v1/auth/register/local/customer`, data)
         
         setRegisterResponse({
             success: true,
-            message: registerResponse.data.message
+            message: 'Usuario creado correctamente'
         })
 
         setRegisterError(null)
@@ -45,7 +45,7 @@ const handleRegister = async (e) => {
         if (err.status === 409) {
             return setRegisterError({
                 success: false,
-                message: 'El nombre de usuario ya existe'
+                message: 'No ha sido posible crear un usuario con este correo electrónico'
             })
         }
 
@@ -60,7 +60,7 @@ const handleRegister = async (e) => {
 
         return setRegisterError({
             success: false,
-            message: 'Error interno, por favor intente nuevamente en unos minutos'
+            message: 'Error interno, por favor intentálo en unos minutos'
         })
     }
 }
