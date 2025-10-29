b1022329b8f3e31541901bef87a29386aaa9f052
Sergio N <s.navarroredondo@gmail.com>
2025-10-29 12:30:07 +0100

    INCREASE timeout on API calls

--- src/config/API/axiosConfig.mjs
+++ src/config/API/axiosConfig.mjs
@@ -4,7 +4,7 @@ import axios from 'axios'
 const apiClient = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true,
-    timeout: 5000, // 5 seconds
+    timeout: 10000, // 5 seconds
     headers: {'Content-Type': 'application/json'}
 });
 
